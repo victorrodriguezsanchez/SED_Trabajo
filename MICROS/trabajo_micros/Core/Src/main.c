a5b3bb83735c015df2cd04b85943f6dd8d37e4e2
sararodriguezfernandez <47814826+sararodriguezfernandez@users.noreply.github.com>
2022-01-15 21:33:12 +0100

    Update main.c

--- MICROS/trabajo_micros/Core/Src/main.c
+++ MICROS/trabajo_micros/Core/Src/main.c
@@ -43,8 +43,6 @@
 /* Private variables ---------------------------------------------------------*/
 ADC_HandleTypeDef hadc1;
 
-I2C_HandleTypeDef hi2c1;
-
 TIM_HandleTypeDef htim1;
 
 /* USER CODE BEGIN PV */
@@ -60,7 +58,6 @@ void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
 static void MX_ADC1_Init(void);
 static void MX_TIM1_Init(void);
-static void MX_I2C1_Init(void);
 /* USER CODE BEGIN PFP */
 void subirpersiana();
 void bajarpersiana();
@@ -146,22 +143,18 @@ int map(int st1, int fn1, int st2, int fn2, int value)
 	}
 
 void subirpersiana(){
-	lcd_clear();
 	servo_girar();
 	HAL_Delay(500);
 	HAL_GPIO_WritePin(GPIOD, GPIO_PIN_14, GPIO_PIN_RESET); //led que simboliza la persiana se apaga una vez se sube
 	HAL_Delay(200);
-	lcd_send_string("SUBIDA");
 	subir=0;
 }
 
 void bajarpersiana(){
-	lcd_clear();
 	servo_girar();
 	HAL_Delay(500);
 	HAL_GPIO_WritePin(GPIOD, GPIO_PIN_15, GPIO_PIN_SET);
 	HAL_Delay(200);
-	lcd_send_string("BAJADA");
 	bajar=0;
 	}
 void servo_write(int angle)
@@ -221,10 +214,8 @@ int main(void)
   MX_GPIO_Init();
   MX_ADC1_Init();
   MX_TIM1_Init();
-  MX_I2C1_Init();
   /* USER CODE BEGIN 2 */
-  lcd_init();
-  lcd_send_string("HOLA");
+
 
 
   /* USER CODE END 2 */
@@ -362,40 +353,6 @@ static void MX_ADC1_Init(void)
 }
 
 /**
-  * @brief I2C1 Initialization Function
-  * @param None
-  * @retval None
-  */
-static void MX_I2C1_Init(void)
-{
-
-  /* USER CODE BEGIN I2C1_Init 0 */
-
-  /* USER CODE END I2C1_Init 0 */
-
-  /* USER CODE BEGIN I2C1_Init 1 */
-
-  /* USER CODE END I2C1_Init 1 */
-  hi2c1.Instance = I2C1;
-  hi2c1.Init.ClockSpeed = 100000;
-  hi2c1.Init.DutyCycle = I2C_DUTYCYCLE_2;
-  hi2c1.Init.OwnAddress1 = 0;
-  hi2c1.Init.AddressingMode = I2C_ADDRESSINGMODE_7BIT;
-  hi2c1.Init.DualAddressMode = I2C_DUALADDRESS_DISABLE;
-  hi2c1.Init.OwnAddress2 = 0;
-  hi2c1.Init.GeneralCallMode = I2C_GENERALCALL_DISABLE;
-  hi2c1.Init.NoStretchMode = I2C_NOSTRETCH_DISABLE;
-  if (HAL_I2C_Init(&hi2c1) != HAL_OK)
-  {
-    Error_Handler();
-  }
-  /* USER CODE BEGIN I2C1_Init 2 */
-
-  /* USER CODE END I2C1_Init 2 */
-
-}
-
-/**
   * @brief TIM1 Initialization Function
   * @param None
   * @retval None
@@ -484,7 +441,6 @@ static void MX_GPIO_Init(void)
   __HAL_RCC_GPIOA_CLK_ENABLE();
   __HAL_RCC_GPIOE_CLK_ENABLE();
   __HAL_RCC_GPIOD_CLK_ENABLE();
-  __HAL_RCC_GPIOB_CLK_ENABLE();
 
   /*Configure GPIO pin Output Level */
   HAL_GPIO_WritePin(GPIOD, GPIO_PIN_12|GPIO_PIN_13|GPIO_PIN_14|GPIO_PIN_15, GPIO_PIN_RESET);
